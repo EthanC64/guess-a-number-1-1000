23527ea1be56b78df6a68f557cd687d91683aa30
EthanC64 <64redbrick@gmail.com>
2022-04-03 08:39:27 -0700

    hello

--- scripts.js
+++ scripts.js
@@ -1,29 +1,86 @@
-console.log("scriptIsrunning");
-console.log(text);
-if (theRandomNumber == guessNumber) {
-  alert("correct");
-}
+var tries = 0;
+var wins = 0;
+// computer picks a number
+var computerPick = Math.floor(Math.random() * 1000);
+console.log(computerPick);
 
-if (theRandomNumber > guessNumber) {
-  alert("too low");
-}
+var playGame = function (e) {
+  e.preventDefault();
 
-if (theRandomNumber < guessNumber) {
-  alert("too high");
-}
+  // user picks a number between
+  var userPick = document.querySelector("#user-input").value;
+  console.log("userPick", userPick, computerPick);
 
-var guessNumber = prompt("guess a number 1-1000");
+  // update tries
+  tries++;
+  document.querySelector("#tries").textContent = tries;
 
-if (theRandomNumber == guessNumber) {
-  alert("correct");
-}
+  console.log(tries, tries >= 10);
+  if (tries >= 10) {
+    // show message
+    document.querySelector("#message-box").textContent =
+      "You lose, sell your computer!";
 
-if (theRandomNumber > guessNumber) {
-  alert("too low");
-}
+    // reset tries
+    tries = 0;
 
-if (theRandomNumber < guessNumber) {
-  alert("too high");
-}
+    // pick new number
+    computerPick = Math.floor(Math.random() * 1000);
+    console.log(computerPick);
 
-document.querySelector(".guessedNum").value;
+    return;
+  }
+
+  // determine a winner and add to winner
+  if (userPick == computerPick) {
+    // show message
+    document.querySelector("#message-box").textContent = "You win!";
+
+    //reset Tries
+    tries = 0;
+
+    // add on the win count
+    wins++;
+    document.querySelector("#wins").textContent = wins;
+  } else {
+    if (userPick > computerPick) {
+      document.querySelector("#message-box").textContent = "Too High!";
+    } else {
+      document.querySelector("#message-box").textContent = "Too Low!";
+    }
+  }
+};
+
+document.querySelector("#submit").addEventListener("click", playGame);
+
+// add to number tries left - alert you lose and reset the game
+
+// console.log("scriptIsrunning");
+// console.log(text);
+// if (theRandomNumber == guessNumber) {
+//   alert("correct");
+// }
+
+// if (theRandomNumber > guessNumber) {
+//   alert("too low");
+// }
+
+// if (theRandomNumber < guessNumber) {
+//   alert("too high");
+// }
+
+// var guessNumber = prompt("guess a number 1-1000");
+
+// if (theRandomNumber == guessNumber) {
+//   alert("correct");
+// }
+
+// if (theRandomNumber > guessNumber) {
+//   alert("too low");
+// }
+
+// if (theRandomNumber < guessNumber) {
+//   alert("too high");
+// }
+
+// document.querySelector(".guessedNum").value;
